c30730f5065cdff5abe16c5caca1c908efc64d5d
sbs87 <sbs@stevenbsmith.net>
2020-04-04 22:44:45 -0400

    lagged on new case/death variable instead of cumulative

--- merge_data_sources-steve.R
+++ merge_data_sources-steve.R
@@ -84,17 +84,26 @@ for(i in 1:nrow(master_table.filter)){
   #i<-1
   day_before_index.i<-master_table.filter[i,"day_before_index"]
   UID.i<-master_table.filter[i,"Unique_ID"]
+  today_case<-master_table.filter[i,"cases"]
+  today_death<-master_table.filter[i,"deaths"]
   day_before_cases<-NA
+  day_before_deaths<-NA
   if(min( master_table.filter[master_table.filter$Unique_ID==UID.i,"date"])>day_before_index.i){
     day_before_cases<-NA
   }else{
-    day_before_cases<-master_table.filter[master_table.filter$Unique_ID==UID.i & master_table.filter$date==day_before_index.i,"cases"][1]
-  
-  }
+    day_before_cases<-master_table.filter[master_table.filter$Unique_ID==UID.i & master_table.filter$date==day_before_index.i,"New_Cases"][1]
+    day_before_deaths<-master_table.filter[master_table.filter$Unique_ID==UID.i & master_table.filter$date==day_before_index.i,"New_Deaths"][1]
+   cases_perday<-today_case-master_table.filter[master_table.filter$Unique_ID==UID.i & master_table.filter$date==day_before_index.i,"cases"][1]
+  deaths_perday<-today_death-master_table.filter[master_table.filter$Unique_ID==UID.i & master_table.filter$date==day_before_index.i,"deaths"][1]
+      }
 if(is.null(length(day_before_cases))){
   day_before_cases<-NA
+  day_before_deaths<-NA
 }
   master_table.filter[i,"day_before_cases"]<-day_before_cases
+  master_table.filter[i,"day_before_deaths"]<-day_before_deaths
+  #master_table.filter[i,"cases_perday"]<-cases_perday
+  #master_table.filter[i,"deaths_perday"]<-deaths_perday
 }
 
 head(master_table.filter)
@@ -102,8 +111,43 @@ head(master_table.filter)
 
 ggplot(master_table.filter,aes(x=date,y=day_before_cases))+geom_point()+
   geom_point(aes(y=cases),col='red')
+  
+  ggplot(master_table.filter,aes(x=date,y=day_before_deaths))+geom_point()+
+  geom_point(aes(y=deaths),col='red')
 
 
 write.csv(master_table.filter,"/Users/stevensmith/Projects/MIT_COVID19/output/MASTER_filtered_withlag.csv",quote = F,row.names = F)
 master_table.filter$Total_Hospital_Beds
 ggplot(master_table.filter,aes(x=Nearest_BigCity,y=Potentially_Available_Hospital_Beds))+geom_point()
+
+write.csv(ddply(master_table.filter,c("Unique_ID"),summarise,max_date=max(as.Date(date)),max_date_mins_1=max(as.Date(date))-1))
+
+X<-read.csv("/Users/stevensmith/Projects/MIT_COVID19/output/Training_Set_X.csv") %>% select(-c("X"))
+Y<-read.csv("/Users/stevensmith/Projects/MIT_COVID19/output/Training_Set_Y.csv") %>% select(-c("X"))
+
+nrow(X)
+
+training_inx<-sample(nrow(X),replace = F,size = nrow(X)*.70)
+
+
+install.packages("randomForest")
+library(randomForest)
+testing_idx<-(1:nrow(X))[!1:nrow(X) %in% training_inx]
+rf.model.training<-randomForest(x=X[training_inx,],y=Y[training_inx,])
+data.frame(feature=row.names(importance(rf.model)),importance(rf.model)) %>% arrange(IncNodePurity)
+rf.model.training
+
+predicted<-predict(rf.model.training,X[testing_idx,])
+plot((predicted-Y$cases_normalized[testing_idx])^2)
+
+plot(Y$cases_normalized,X$Pop_Density)
+
+
+
+X.vaiation<-read.csv("/Users/stevensmith/Projects/MIT_COVID19/output/Holdout_Set_X.csv") %>% select(-c("X"))
+Y.vaiation<-read.csv("/Users/stevensmith/Projects/MIT_COVID19/output/Holdout_Set_Y.csv") %>% select(-c("X"))
+predicted.validation<-predict(rf.model.training,X.vaiation)
+plot((predicted.validation-Y.vaiation$cases_normalized)^2)
+X.vaiation
+
+data.frame(normalized_predicted_cases=predicted.validation)
